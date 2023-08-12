3e5361992f3eba5feeeb82190457e43d5e3db924
umairmayo80 <aliumer1080@gmail.com>
2023-08-12 21:36:23 +0100

    Typescript intro update

--- TypeScript Basics/Intro.ts
+++ TypeScript Basics/Intro.ts
@@ -1,6 +1,6 @@
 console.log("Hi Umair")
 
-console.log(name1); // typescript is helping us to identify potential errors at development time/ compile time
+console.log(name1); // typescript is helping us to identify potential errors at development time/ compile time.
 
 
 
