69382fc3b248dc15491dfc4a187f03edac3db346
umairmayo80 <aliumer1080@gmail.com>
2023-08-27 00:21:08 +0500

    Decorators in Typescript with example

--- TypeScript Basics/decorators.ts
+++ TypeScript Basics/decorators.ts
@@ -3,3 +3,21 @@
 // Decorators are applied using the @ symbol followed by the decorator name above the target entity. The decorator function is then executed with information about the target and can modify it if needed. Decorators can be used with classes, class methods, class properties, and method parameters.
 
 // Here's a basic example of a class decorator:
+
+function MyClassDecorator(target, temp) {
+    console.log("Class decorator called on:", target);
+    console.log("Class decorator called on:", temp);
+}
+
+@MyClassDecorator
+class MyClass {
+    constructor() {
+        console.log("MyClass constructor");
+    }
+}
+
+const myInstance = new MyClass(); // Output: "Class decorator called on:" [Function: MyClass]
+                                  //         "MyClass constructor"
+
+
+export {};
